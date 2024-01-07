4c2b225a53c3b1c2e0ad1438b0151f3d42af1ee0
tuilakie <ntneik15@gmail.com>
2024-01-07 18:22:48 +0700

    feat: improve performance

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,33 +20,56 @@ export class AppService {
     try {
       const soNguoiBanMoi = +this.configService.get('SO_NGUOI_BAN_MOI');
       const soNguoiBan = 1 + soNguoiBanMoi;
-      const tongSoSanPham = await this.httpService
-        .get('/wp-json/wc/v3/reports/products/totals')
-        .toPromise()
-        .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0));
-      const soSanPhamMoi = +this.configService.get('SO_SAN_PHAM_MOI');
-      const soLuongGiaoDich = await this.httpService
-        .get('/wp-json/wc/v3/reports/orders/totals')
-        .toPromise()
-        .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0));
-      const tongSoDonHangThanhCong = await this.httpService
-        .get('/wp-json/wc/v3/reports/orders/totals')
-        .toPromise()
-        .then((res) => _.find(res.data, { slug: 'completed' })?.total);
-      const tongSoDonHangKhongThanhCong =
-        (await this.httpService
-          .get('/wp-json/wc/v3/reports/orders/totals')
-          .toPromise()
-          .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0))) -
-        tongSoDonHangThanhCong;
       const date_min = this.configService.get('GOV_DATE_MIN');
       const date_max = new Date().toISOString().split('T')[0];
-      const tongGiaTriGiaoDich = +(await this.httpService
-        .get(
-          `https://cemcons.vn/wp-json/wc/v3/reports/sales?date_min=${date_min}&date_max=${date_max}`,
-        )
-        .toPromise()
-        .then((res) => res.data[0]?.total_sales));
+
+      const [products, orders, sales] = await Promise.all([
+        this.httpService
+          .get('/wp-json/wc/v3/reports/products/totals')
+          .toPromise()
+          .then((res) => res.data),
+        this.httpService
+          .get('/wp-json/wc/v3/reports/orders/totals')
+          .toPromise()
+          .then((res) => res.data),
+        this.httpService
+          .get(
+            `https://cemcons.vn/wp-json/wc/v3/reports/sales?date_min=${date_min}&date_max=${date_max}`,
+          )
+          .toPromise()
+          .then((res) => res.data),
+      ]);
+
+      console.log(products, orders, sales);
+      // const tongSoSanPham = await this.httpService
+      //   .get('/wp-json/wc/v3/reports/products/totals')
+      //   .toPromise()
+      //   .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0));
+      const tongSoSanPham = _.reduce(products, (sum, n) => sum + n?.total, 0);
+      const soSanPhamMoi = +this.configService.get('SO_SAN_PHAM_MOI');
+      // const soLuongGiaoDich = await this.httpService
+      //   .get('/wp-json/wc/v3/reports/orders/totals')
+      //   .toPromise()
+      //   .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0));
+      const soLuongGiaoDich = _.reduce(orders, (sum, n) => sum + n?.total, 0);
+      // const tongSoDonHangThanhCong = await this.httpService
+      //   .get('/wp-json/wc/v3/reports/orders/totals')
+      //   .toPromise()
+      //   .then((res) => _.find(res.data, { slug: 'completed' })?.total);
+      const tongSoDonHangThanhCong = _.find(orders, {
+        slug: 'completed',
+      })?.total;
+      // const tongSoDonHangKhongThanhCong =
+      //   (await this.httpService
+      //     .get('/wp-json/wc/v3/reports/orders/totals')
+      //     .toPromise()
+      //     .then((res) => _.reduce(res.data, (sum, n) => sum + n?.total, 0))) -
+      //   tongSoDonHangThanhCong;
+      const tongSoDonHangKhongThanhCong =
+        _.reduce(orders, (sum, n) => sum + n?.total, 0) -
+        tongSoDonHangThanhCong;
+
+      const tongGiaTriGiaoDich = +sales[0]?.total_sales;
 
       return {
         soLuongTruyCap: +this.configService.get('SO_LUONG_TRUY_CAP'),
